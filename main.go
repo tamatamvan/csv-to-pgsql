a0561c42160b1be751b19494d549a7092c9195bc
tamatamvan <sfirst.tama@gmail.com>
2024-01-31 19:35:21 +0700

    fix replace deprecated io/ioutils

--- main.go
+++ main.go
@@ -2,29 +2,28 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
 )
 
 func getCSVsFromDir(dir string) {
-	files, err := ioutil.ReadDir(dir)
+	files, err := os.ReadDir(dir)
 	if err != nil {
-			log.Fatal(err)
+		log.Fatal(err)
 	}
 
 	for _, file := range files {
-			if filepath.Ext(file.Name()) == ".csv" {
-					fmt.Println(file.Name())
-			}
+		if filepath.Ext(file.Name()) == ".csv" {
+			fmt.Println(file.Name())
+		}
 	}
 }
 
 func main() {
 	if len(os.Args) < 2 {
-			fmt.Println("Please provide a directory")
-			os.Exit(1)
+		fmt.Println("Please provide a directory")
+		os.Exit(1)
 	}
 	dir := os.Args[1]
 	getCSVsFromDir(dir)
