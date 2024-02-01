fa46cd1f32827ef7c315cd2cb8f372eff6621f53
tamatamvan <sfirst.tama@gmail.com>
2024-02-01 13:17:42 +0700

    read csv files content

--- main.go
+++ main.go
@@ -1,23 +1,45 @@
 package main
 
 import (
+	"encoding/csv"
 	"fmt"
+	"io/fs"
 	"log"
 	"os"
-	"path/filepath"
+	"strings"
 )
 
-func getCSVsFromDir(dir string) {
+func getCSVsFromDir(dir string) []fs.DirEntry {
 	files, err := os.ReadDir(dir)
 	if err != nil {
 		log.Fatal(err)
 	}
+	return files
+}
 
-	for _, file := range files {
-		if filepath.Ext(file.Name()) == ".csv" {
-			fmt.Println(file.Name())
+func readCSVinDir(dir string) {
+	csvFiles := getCSVsFromDir(dir)
+
+	for _, file := range csvFiles {
+		tableName := strings.TrimSuffix(file.Name(), ".csv")
+		fmt.Println("Table Name: ", tableName)
+
+		contents, err := os.ReadFile(dir + "/" + file.Name())
+
+		if err != nil {
+			log.Fatal(err)
 		}
+		r := csv.NewReader(strings.NewReader(string(contents)))
+		records, err1 := r.ReadAll()
+		if err1 != nil {
+			log.Fatal(err)
+		}
+		columns := records[0]
+		data := records[1:]
+		fmt.Println("Columns: ", columns)
+		fmt.Println("Data: ", data)
 	}
+
 }
 
 func main() {
@@ -26,5 +48,5 @@ func main() {
 		os.Exit(1)
 	}
 	dir := os.Args[1]
-	getCSVsFromDir(dir)
+	readCSVinDir(dir)
 }
