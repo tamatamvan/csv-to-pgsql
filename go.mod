8242bd69143e2ee20875ba74a83a7252a2dbd794
tamatamvan <sfirst.tama@gmail.com>
2024-02-04 01:21:33 +0700

    read csv, return csv contents, and create tables

--- go.mod
+++ go.mod
@@ -3,10 +3,13 @@ module github.com/tamatamvan/csv-to-pgsql
 go 1.21.6
 
 require (
+	github.com/jackc/pgx/v5 v5.5.2
+	github.com/joho/godotenv v1.5.1
+)
+
+require (
 	github.com/jackc/pgpassfile v1.0.0 // indirect
 	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
-	github.com/jackc/pgx/v5 v5.5.2 // indirect
-	github.com/joho/godotenv v1.5.1 // indirect
 	golang.org/x/crypto v0.17.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,15 +3,19 @@ package main
 import (
 	"context"
 	"encoding/csv"
+	"encoding/json"
 	"fmt"
 	"io/fs"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 
-	"github.com/joho/godotenv"
 	"github.com/jackc/pgx/v5"
+	"github.com/joho/godotenv"
 )
+
+var db *pgx.Conn
 
 func getCSVsFromDir(dir string) []fs.DirEntry {
 	files, err := os.ReadDir(dir)
@@ -21,12 +25,65 @@ func getCSVsFromDir(dir string) []fs.DirEntry {
 	return files
 }
 
-func readCSVinDir(dir string) {
+func inferDataType(data string) string {
+	if _, err := strconv.ParseInt(data, 10, 64); err == nil {
+		return "int"
+	}
+	if _, err := strconv.ParseFloat(data, 64); err == nil {
+		return "decimal(10,2)"
+	}
+	var arr []string
+	if err := json.Unmarshal([]byte(data), &arr); err == nil {
+		return "text[]"
+	}
+	// If it's not any of the above types, return string
+	return "varchar(255)"
+}
+
+func inferColumnTypes(columns []string, data []string) []string {
+	var colTypes []string
+	for i := 0; i < len(data); i++ {
+		var inferredType string
+		if columns[i] == "password" {
+			inferredType = "varchar(255)"
+		} else {
+			inferredType = inferDataType(data[i])
+		}
+		colTypes = append(colTypes, columns[i]+" "+inferredType)
+	}
+	return colTypes
+}
+
+func createTable(csvContent CSVContent) {
+	// check if table if not exists
+	// Convert the columns slice to a comma-separated string
+	cols := strings.Join(inferColumnTypes(csvContent.Columns, csvContent.Data[0]), ", ")
+
+	// Prepare the SQL statement
+	sqlStatement := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s (%s);`, csvContent.TableName, cols)
+
+	fmt.Println(sqlStatement)
+	// Execute the SQL statement
+	_, err := db.Exec(context.Background(), sqlStatement)
+	if err != nil {
+		log.Fatal(err)
+		os.Exit(2)
+	}
+
+}
+
+type CSVContent struct {
+	TableName string
+	Columns   []string
+	Data      [][]string
+}
+
+func readCSVinDir(dir string) []CSVContent {
 	csvFiles := getCSVsFromDir(dir)
+	var csvContents []CSVContent
 
 	for _, file := range csvFiles {
 		tableName := strings.TrimSuffix(file.Name(), ".csv")
-		fmt.Println("Table Name: ", tableName)
 
 		contents, err := os.ReadFile(dir + "/" + file.Name())
 
@@ -40,27 +97,17 @@ func readCSVinDir(dir string) {
 		}
 		columns := records[0]
 		data := records[1:]
-		fmt.Println("Columns: ", columns)
-		fmt.Println("Data: ", data)
+
+		csvContent := CSVContent{
+			TableName: tableName,
+			Columns:   columns,
+			Data:      data,
+		}
+
+		csvContents = append(csvContents, csvContent)
 	}
 
-}
-
-func createTable(tableName string, columns []string) {
-	// check if table if not exists
-	// Convert the columns slice to a comma-separated string
-	cols := strings.Join(columns, ", ")
-
-	// Prepare the SQL statement
-	sqlStatement := fmt.Sprintf(`CREATE TABLE IF NOT EXISTS %s (%s);`, tableName, cols)
-
-	// Execute the SQL statement
-	_, err := conn.Exec(context.Background(), sqlStatement)
-	if err != nil {
-			return err
-	}
-
-	return nil
+	return csvContents
 }
 
 func init() {
@@ -72,7 +119,8 @@ func init() {
 		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
 		os.Exit(1)
 	}
-	defer conn.Close(context.Background())
+	db = conn
+	// defer db.Close(context.Background())
 
 }
 
@@ -82,5 +130,11 @@ func main() {
 		os.Exit(1)
 	}
 	dir := os.Args[1]
-	readCSVinDir(dir)
+
+	csvContents := readCSVinDir(dir)
+
+	for _, csvContent := range csvContents {
+		fmt.Println("Table Name: ", csvContent.TableName)
+		createTable(csvContent)
+	}
 }
